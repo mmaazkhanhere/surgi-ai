3c13355fede33cd584bcfeb3262d570da7583cba
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-09-14 16:24:25 +0500

    creating a rag tool to query pinecone

---
+++ helper_functions/pinecone_vector_store.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+from pinecone import Pinecone
+
+load_dotenv()
+
+def pinecone_vector_store():
+    pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+    pc = Pinecone(api_key=pinecone_api_key)
+    index_name = "surgical-assistant"
+    index = pc.Index(index_name)
+
+    embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    vector_store = PineconeVectorStore(index=index, embedding=embedding)
+
+    return vector_store
+
+def embeddings():
+    embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    return embedding
