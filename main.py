414de0fb16446b33a5facf076f2cee6f21775ef3
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-09-14 11:02:29 +0500

    basic during surgery crew created

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
+from crews.during_surgery_crew import during_surgery_crew
+
 st.title('SurgiAI')
+
+surgeony_query = st.text_input('Enter your query')
+patient_history = st.text_area('Enter patient history')
+
+btn = st.button('Ask')
+
+if btn:
+    response = during_surgery_crew(surgeony_query, patient_history)
+    st.write(response)
