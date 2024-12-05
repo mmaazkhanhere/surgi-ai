0ed838e2cf7052e139597756ddea133a12a738ae
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2024-12-05 19:50:44 +0500

    backend fastapi endpoint tested (#43)
    * frontend for during surgery created
    
    * fastapi endpoint for during surgery working

--- backend/new/main.py
+++ backend/new/main.py
@@ -1,5 +1,8 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+from agent import surgical_agent
 
 class DuringSurgery(BaseModel):
     surgeon_query: str
@@ -7,11 +10,30 @@ class DuringSurgery(BaseModel):
 
 app: FastAPI = FastAPI()
 
-app.get('/')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/')
 async def root():
-    return {"message": "SurgiAI"}
+    return {"message": "Welcome to SurgiAI"}
 
 
-app.post('/surgery')
-async def surgery(input: DuringSurgery):
-    return {"message": "Surgery"}
+@app.post('/surgery')
+async def surgical_query(input: DuringSurgery):
+    state = {
+        'surgeon_query': input.surgeon_query,
+        'patient_history': input.patient_history,
+        'conversation': [],
+        'messages': [],
+        'insight_accumulator_response': '',
+        'max_iteration': 2,
+        'current_iteration': 0
+    }
+    response = surgical_agent(state)
+    return response
