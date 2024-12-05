ac6f283df7a122e4713d4c22466befc5d43fbc15
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2024-12-05 18:09:10 +0500

    agent created in background. fastapi endpoints created (#41)
    * creating fastapi endpoint
    
    * adding nodes and router function
    
    * agent created in the backend. fastapi endpoints created

--- backend/new/main.py
+++ backend/new/main.py
@@ -1,1 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class DuringSurgery(BaseModel):
+    surgeon_query: str
+    patient_history: str
+
+app: FastAPI = FastAPI()
+
+app.get('/')
+async def root():
+    return {"message": "SurgiAI"}
+
+
+app.post('/surgery')
+async def surgery(input: DuringSurgery):
+    return {"message": "Surgery"}
